f3a72d3b18b317f011431fc4fed72cd5614b8c3f
madboy482 <73926361+madboy482@users.noreply.github.com>
2021-07-08 10:33:37 +0530

    K

---
+++ bot.py
@@ -0,0 +1,108 @@
+from telethon import events, TelegramClient, Button
+import logging
+from telethon.tl.functions.users import GetFullUserRequest as us
+import os
+
+
+logging.basicConfig(level=logging.INFO)
+
+TOKEN = os.environ.get("TOKEN", None)
+
+bot = TelegramClient(
+        "Messenger",
+        api_id=6,
+        api_hash="eb06d4abfb49dc3eeb1aeb98ae0f581e"
+        ).start(
+                bot_token=TOKEN
+                )
+db = {}
+
+@bot.on(events.NewMessage(pattern="^[!?/]start$"))
+async def stsrt(event):
+    await event.reply(
+            "**Heya, I am a MessengerBot!**",
+            buttons=[
+                [Button.switch_inline("Go Inline", query="")]
+                ]
+            )
+
+
+@bot.on(events.InlineQuery())
+async def die(event):
+    if len(event.text) != 0:
+        return
+    me = (await bot.get_me()).username
+    dn = event.builder.article(
+            title="It's a MessengerBot!",
+            description="It's a MessengerBot!\n(C) MadBoy482",
+            text=f"**It's a MessengerBot**\n`@{me} wspr UserID|Message`\n**(C) MadBoy482**",
+            buttons=[
+                [Button.switch_inline(" Go Inline ", query="wspr ")]
+                ]
+            )
+    await event.answer([dn])
+    
+@bot.on(events.InlineQuery(pattern="wspr"))
+async def inline(event):
+    me = (await bot.get_me()).username
+    try:
+        inp = event.text.split(None, 1)[1]
+        user, msg = inp.split("|")
+    except IndexError:
+        await event.answer(
+                [], 
+                switch_pm=f"@{me} [UserID]|[Message]",
+                switch_pm_param="start"
+                )
+    except ValueError:
+        await event.answer(
+                [],
+                switch_pm=f"Give a message too!",
+                switch_pm_param="start"
+                )
+    try:
+        ui = await bot(us(user))
+    except BaseException:
+        await event.answer(
+                [],
+                switch_pm="Invalid User ID/Username",
+                switch_pm_param="start"
+                )
+        return
+    db.update({"user_id": ui.user.id, "msg": msg, "self": event.sender.id})
+    text = f"""
+**📩 Sᴇᴄʀᴇᴛ Msɢ** To **[{ui.user.first_name}](tg://user?id={ui.user.id})**.
+Oɴʟʏ Hᴇ/Sʜᴇ Cᴀɴ Oᴘᴇɴ Iᴛ.."
+    """
+    dn = event.builder.article(
+            title="Its a secret message! Sssh",
+            description="It's a secret message! Sssh!",
+            text=text,
+            buttons=[
+                [Button.inline("🔐 Sʜᴏᴡ", data="wspr")]
+                ]
+            )
+    await event.answer(
+            [dn],
+            switch_pm="It's a secret message! Sssh",
+            switch_pm_param="start"
+            )
+
+
+@bot.on(events.CallbackQuery(data="wspr"))
+async def ws(event):
+    user = int(db["user_id"])
+    lol = [int(db["self"])]
+    lol.append(user)
+    if event.sender.id not in lol:
+        await event.answer("Yes You, Little Shit, Why're u seeing my msg??", alert=False)
+        return
+    msg = db["msg"]
+    if msg == []:
+        await event.anwswer(
+                "Oops!\nIt's looks like message got deleted from my server!", alert=True)
+        return
+    await event.answer(msg, alert=True)
+
+print("Succesfully Started Bot!")
+bot.run_until_disconnected()
